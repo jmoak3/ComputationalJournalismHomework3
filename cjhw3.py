7f024efd2ef1756dd1de828d434d9bd32cde3b7a
jmoak3 <mrjmoak3@gmail.com>
2016-03-19 22:26:26 -0700

    progress

---
+++ cjhw3.py
@@ -0,0 +1,28 @@
+from chroniclingamerica import ChronAm
+from pprint import pprint
+
+
+def getStates(fetcher):
+	table = {}
+	for item in fetcher.fetch():
+		if "state" in item:
+			key = item["state"][0]
+			if key in table:
+				table[key] += 1
+			else: 
+				table[key] = 1
+	return table
+
+search_term = "witch"
+search_term2 = "witches"
+
+fetcher1 = ChronAm(search_term, 1,500)
+fetcher2 = ChronAm(search_term2,1,500)
+statesTable = {}
+statesTable.update(getStates(fetcher1))
+statesTable.update(getStates(fetcher2))
+print(statesTable)
+f = open('newspaper.csv', 'w')
+for key, value in statesTable.items():
+	f.write('{0},{1}\n'.format(key, value))
+#print(mm)
